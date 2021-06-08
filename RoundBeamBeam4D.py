Fixed bug with average position in the soft-Gaussian 4D beam-beam

RoundBeamBeam4D.py
@@ -9,15 +9,16 @@ class RoundBeamBeam4D(PyPLINEDElement):
         key = self.getMessageKey(beam.ID,partnerIDs[0])
         requestIsPending = False
         if key in self._pendingRequests.keys():
-            print('There is a pending request with key',key)
-            if not self._pendingRequests[key].Test():
+            if beam.period == self._pendingRequests[key]:
+                #print('There is a pending request with key',key,'at turn',beam.period)
                 requestIsPending = True
         if not requestIsPending:
-            print(beam.ID.name,'sending avg to',partnerIDs[0].name,'with tag',tag)
+            #print(beam.ID.name,'sending avg to',partnerIDs[0].name,'with tag',tag)
             if not beam.isReal:
                 print('Cannot compute avg on fake beam')
             params = np.array([beam.mean_x(),beam.mean_y(),beam.sigma_x(),beam.sigma_y(),beam.intensity])
-            self._pendingRequests[key] = self._comm.Isend(params,dest=partnerIDs[0].rank,tag=tag)
+            self._comm.Isend(params,dest=partnerIDs[0].rank,tag=tag)
+            self._pendingRequests[key] = beam.period
 
     def messagesAreReady(self,beamID, partnerIDs):
         return self._comm.Iprobe(source=partnerIDs[0].rank, tag=self.getMessageTag(partnerIDs[0],beamID))
@@ -26,12 +27,10 @@ class RoundBeamBeam4D(PyPLINEDElement):
         tag = self.getMessageTag(partnerIDs[0],beam.ID)
         params =  np.empty(5, dtype=np.float64)
         self._comm.Irecv(params,source=partnerIDs[0].rank,tag=tag)
-        print(beam.ID.name,'revieved avg from',partnerIDs[0].name,'with tag',tag,params)
+        #print(beam.ID.name,'revieved avg from',partnerIDs[0].name,'with tag',tag,params)
         sigma = (params[2]+params[3])/2
-        myX = beam.mean_x()
-        myY = beam.mean_y()
-        r2 = (params[0]-myX)**2 + (params[1]-myY)**2
+        r2 = (params[0]-beam.x)**2 + (params[1]-beam.y)**2
         r0 = beam.charge**2/(4*np.pi*cst.epsilon_0*beam.mass*cst.c**2) #TODO lift assumption that both beams have the particle type
-        common = -2*r0*params[4]*(1-np.exp(-0.5*r2/sigma**2))/beam.gamma #TODO lift assumption that both beams have the same gamma
-        beam.xp += common*(params[0]-myX)/r2
-        beam.yp += common*(params[1]-myY)/r2
+        common = -2*r0*params[4]*(1-np.exp(-0.5*r2/sigma**2))/beam.gamma/r2 #TODO lift assumption that both beams have the same gamma
+        beam.xp += common*(params[0]-beam.x)
+        beam.yp += common*(params[1]-beam.y)
Fix sign of delay and propagate the slice set beta via MPI

PyPLINE/PyPLINEDWakeField.py
@@ -12,7 +12,6 @@ from PyHEADTAIL.particles.slicing import SliceSet
 class PyPLINEDWakeField(WakeField,PyPLINEDElement):
 
     def __init__(self,name,number,slicer,*wake_sources):
-        print(*wake_sources,wake_sources)
         PyPLINEDElement.__init__(self,name,number)
         WakeField.__init__(self,slicer,*wake_sources)
 
@@ -25,7 +24,7 @@ class PyPLINEDWakeField(WakeField,PyPLINEDElement):
 
         self._attributes_to_buffer = ['n_macroparticles_per_slice']
         self._attributes_to_buffer.extend(self._statistics)
-        bufferSize = self.slicer.n_slices*len(self._attributes_to_buffer)+1
+        bufferSize = self.slicer.n_slices*len(self._attributes_to_buffer)+2
         self._send_buffer = np.zeros(bufferSize,dtype=float)
         self._recv_buffer = np.zeros(bufferSize,dtype=float)
 
@@ -34,6 +33,7 @@ class PyPLINEDWakeField(WakeField,PyPLINEDElement):
         for i in range(len(self._attributes_to_buffer)):
             self._send_buffer[k:k+len(getattr(slice_set,self._attributes_to_buffer[i]))] = getattr(slice_set,self._attributes_to_buffer[i])
             k += self.slicer.n_slices
+        self._send_buffer[-2] = slice_set.beta
         self._send_buffer[-1] = delay
 
         self.tmp = copy.deepcopy(slice_set.slice_index_of_particle)
@@ -47,7 +47,7 @@ class PyPLINEDWakeField(WakeField,PyPLINEDElement):
             k += self.slicer.n_slices
         slice_set_kwargs['n_macroparticles_per_slice'] = arrays_recvd['n_macroparticles_per_slice'].astype(int)
         slice_set_kwargs['slice_index_of_particle'] = None
-        slice_set_kwargs['beam_parameters'] = {'beta':slice_set0.beta}
+        slice_set_kwargs['beam_parameters'] = {'beta':self._recv_buffer[-2]}
         slice_set = SliceSet(**slice_set_kwargs)
         for statistic in self._statistics:
             setattr(slice_set,statistic,arrays_recvd[statistic])
@@ -95,7 +95,7 @@ class PyPLINEDWakeField(WakeField,PyPLINEDElement):
             self._comm.Recv(self._recv_buffer,source=partnerID.rank,tag=tag)
             partner_slice_set,partner_delay = self._slice_set_from_buffer(slice_set)
             self.slice_set_deque[beam.ID.number].appendleft(partner_slice_set)
-            self.slice_set_age_deque[beam.ID.number].appendleft(partner_delay-beam.delay)
+            self.slice_set_age_deque[beam.ID.number].appendleft(beam.delay-partner_delay)
         # adding my own slice set (note: my own slice set needs to be the first in the list since PyHEADTAIL uses its 'slice_index_of_particle')
         self.slice_set_deque[beam.ID.number].appendleft(slice_set)
         self.slice_set_age_deque[beam.ID.number].appendleft(0.0)

examples/multibunchInstability_1beam1train.py
@@ -28,8 +28,9 @@ from PyPLINE.PyPLINEDWakeField import PyPLINEDWakeField
 
 context = xo.ContextCpu(omp_num_threads=0)
 myRank = MPI.COMM_WORLD.Get_rank()
-nTurn = int(1E5)
+nTurn = int(1E4)
 nBunch = 8
+bunch_spacing = 25E-9
 bunch_intensity = 1.8E11
 n_macroparticles = int(1E4)
 energy = 7E3 # [GeV]
@@ -55,7 +56,7 @@ sigma_delta = Q_s*sigma_z/(averageRadius*eta)
 beta_s = sigma_z/sigma_delta
 emit_s = 4*np.pi*sigma_z*sigma_delta*p0/constants.e # eVs for PyHEADTAIL
 
-n_slices_wakes = 200
+n_slices_wakes = 10
 limit_z = 3 * sigma_z
 wakefile = '/afs/cern.ch/work/x/xbuffat/PyCOMPLETE/PyPLINE/examples/wakes/wakeforhdtl_PyZbase_Allthemachine_7000GeV_B1_2021_TeleIndex1_wake.dat'
 slicer_for_wakefields = UniformBinSlicer(n_slices_wakes, z_cuts=(-limit_z, limit_z))
@@ -84,7 +85,7 @@ arc = xt.LinearTransferMatrix(alpha_x_0 = 0.0, beta_x_0 = beta_x, disp_x_0 = 0.0
 #                           detx_x = detx_x,detx_y = detx_y,dety_y = detx_x, dety_x = detx_y,
 #                           energy_ref_increment=0.0,energy_increment=0)
 particles_list = []
-for bunchNumber in range(nBunch):
+for bunch_number in range(nBunch):
     particles_list.append(PyPLINEDParticles(circumference=circumference,particlenumber_per_mp=bunch_intensity/n_macroparticles,
                              _context=context,
                              q0 = 1,
@@ -96,18 +97,18 @@ for bunchNumber in range(nBunch):
                              py=np.sqrt(normemit/beta_y/gamma/betar)*np.random.randn(n_macroparticles),
                              zeta=sigma_z*np.random.randn(n_macroparticles),
                              delta=sigma_delta*np.random.randn(n_macroparticles),
-                             name=f'B1b{bunchNumber}',rank=bunchNumber,number=bunchNumber
+                             name=f'B1b{bunch_number}',rank=bunch_number,number=bunch_number,delay=bunch_number*bunch_spacing
                              )
                     )
 
-for bunchNumber in range(nBunch):
+for bunch_number in range(nBunch):
     partnerIDs = []
-    for partnerBunchNumber in range(nBunch):
-        if partnerBunchNumber != bunchNumber:
-            partnerIDs.append(particles_list[partnerBunchNumber].ID)
-    particles_list[bunchNumber].addElementToPipeline(arc)
-    particles_list[bunchNumber].addElementToPipeline(wake_field,partnerIDs)
-    particles_list[bunchNumber].addElementToPipeline(BunchMonitor(filename=f'Multibunch_B1b{bunchNumber}',n_steps=nTurn))
+    for partner_bunch_number in range(nBunch):
+        if partner_bunch_number != bunch_number:
+            partnerIDs.append(particles_list[partner_bunch_number].ID)
+    particles_list[bunch_number].addElementToPipeline(arc)
+    particles_list[bunch_number].addElementToPipeline(wake_field,partnerIDs)
+    particles_list[bunch_number].addElementToPipeline(BunchMonitor(filename=f'Multibunch_B1b{bunch_number}',n_steps=nTurn))
 
 my_particles_list = []
 for particles in particles_list:
@@ -121,14 +122,14 @@ timeAtLastPrint = time.time()
 while not abort:
     atLeastOneBunchIsActive = False
     if myRank == 0:
-        if particles_list[0].period - turnAtLastPrint == 1:
+        if particles_list[0].period - turnAtLastPrint == 100:
             timePerTurn = (time.time()-timeAtLastPrint)/(particles_list[0].period - turnAtLastPrint)
             print(f'Turn {particles_list[0].period}, time per turn {timePerTurn}s',flush=True)
             turnAtLastPrint = particles_list[0].period
             timeAtLastPrint = time.time()
     for particles in my_particles_list:
         if particles.period <= nTurn:
-            particles.step() #TODO gets stuck at first turn
+            particles.step()
             atLeastOneBunchIsActive = True
             
     if not atLeastOneBunchIsActive: